a068b6eae09b7b65c6799823d323923fe465ee7d
Erica <palosaari.erica@gmail.com>
2015-05-02 16:52:53 +0200

    asdasd

--- resources/views/messages/new.blade.php
+++ resources/views/messages/new.blade.php
@@ -1,8 +1,8 @@
 @extends('app')
 @section('content')
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <link media="all" type="text/css" rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
 
     <script style="text/javascript">
         jQuery(document).ready(function() {
