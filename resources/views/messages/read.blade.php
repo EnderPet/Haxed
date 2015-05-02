79043ed552016c9ac60abddb0ad11d6a6bd0bf24
Erica <palosaari.erica@gmail.com>
2015-05-02 16:30:54 +0200

    asdasd

--- resources/views/messages/read.blade.php
+++ resources/views/messages/read.blade.php
@@ -1,4 +1,27 @@
 @extends('app')
 @section('content')
-    Read Message
+    <div class="row">
+        <div class="col-md-6">
+            <div class="" style="overflow:hidden;">
+                <div class="" style="float:left;">Title: Mitt Meddelande<br/>From: MyUser</div>
+                <div class="" style="float:right;">2015-04-02<br/>16:27:00</div>
+            </div>
+            <div class="">
+                <form>
+                    <div class="form-group">
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <textarea rows="4" cols="50" class="form-control"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="Reply" class="form-control">
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="">Hejsan<br/>hejsan<br/>heeeeeeeeeej<br/></div>
+        </div>
+    </div>
 @endsection
