087ce5bc2d20b5dd36e9ca5866155f673d33c3ab
Erica <palosaari.erica@gmail.com>
2015-05-02 15:44:41 +0200

    asdasd

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -3,7 +3,7 @@
 
     <div class="col-md-12">
         <div class="panel panel-default">
-            <div class="panel-heading">Messages</div>
+            <div class="panel-heading">Messages <span class="pull-right"><a href="/messages/new">New Message</a></span></div>
             <div class="panel-body">
                 <div class="row">
                     <div class="col-md-12">
