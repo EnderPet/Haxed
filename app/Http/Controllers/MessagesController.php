425f3cfdbf638ac27ae2108fcc844bd480c6f0f8
Erica <palosaari.erica@gmail.com>
2015-05-02 15:02:06 +0200

    Another controller fix

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,6 +2,12 @@
 
 class MessagesController extends Controller {
 
+    public function __construct(){
+        if(Auth::check()){
+            View::share('user', Auth::user());
+        }
+    }
+
     public function index()
     {
         return view('messages.index');
