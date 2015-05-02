3ce7242016e7afc07e1e4f8975de673aae7c43f9
Erica <palosaari.erica@gmail.com>
2015-05-02 23:54:30 +0200

    asdasd

--- app/Messages.php
+++ app/Messages.php
@@ -6,6 +6,6 @@ class Messages extends Model {
 
     protected $table = 'messages';
 
-    protected $fillable = ['tuid', 'fuid', 'message', 'title'];
+    protected $fillable = ['m_tuid', 'm_fuid', 'm_message', 'm_title'];
 
 }
