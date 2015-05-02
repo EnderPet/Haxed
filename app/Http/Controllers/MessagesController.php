f4dafd062d8743d4184acdcb489b5684f7b64baa
Erica <palosaari.erica@gmail.com>
2015-05-02 15:04:48 +0200

    test

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,12 +2,6 @@
 
 class MessagesController extends Controller {
 
-    public function __construct(){
-        if(Auth::check()){
-            View::share('user', Auth::user());
-        }
-    }
-
     public function index()
     {
         return view('messages.index');
