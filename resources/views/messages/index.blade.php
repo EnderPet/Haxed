9929f40ca9bd3d0026318f8fa3b558fc25356197
Erica <palosaari.erica@gmail.com>
2015-05-02 15:27:52 +0200

    asdasd

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -6,23 +6,10 @@
             <div class="panel-heading">Messages</div>
             <div class="panel-body">
                 <div class="row">
-                    <div class="col-md-5">
+                    <div class="col-md-12">
                         <div class="list-group">
-                            <a href="#" class="list-group-item">Mitt Meddelande</a>
-                            <a href="#" class="list-group-item">Mitt Meddelande 2</a>
-                        </div>
-                    </div>
-                    <div class="col-md-7">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">Mitt Meddelande</div>
-                            <div class="panel-body">
-                                <div class="col-md-12">
-                                    Här är mitt meddelande.
-                                </div>
-                            </div>
-                            <div class="panel-footer">
-                                Från >användare< klockan 18:18 den 20/4/2015
-                            </div>
+                            <a href="#" class="list-group-item">Mitt Meddelande <span style="text-align: right;">From >user<</span></a>
+                            <a href="#" class="list-group-item">Mitt Meddelande 2 <span style="text-align: right;">From >user<</span></a>
                         </div>
                     </div>
                 </div>
