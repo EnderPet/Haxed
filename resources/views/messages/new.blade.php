d4374aabafd8d4506b3e1888e79a360c436592ce
Erica <palosaari.erica@gmail.com>
2015-05-02 16:44:14 +0200

    asdasd

--- resources/views/messages/new.blade.php
+++ resources/views/messages/new.blade.php
@@ -1,6 +1,6 @@
 @extends('app')
 @section('content')
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
 
