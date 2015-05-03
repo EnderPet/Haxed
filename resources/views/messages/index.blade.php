077b18712923ae98d8fe5cbe0ec3f5a83ad0b6cb
Erica <palosaari.erica@gmail.com>
2015-05-03 08:06:14 +0200

    asdasd

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -16,7 +16,7 @@
                         </div>
                     </div>
                     <div class="col-md-12">
-                        {{ $mess->links() }}
+                        {{ $mess->render() }}
                     </div>
                 </div>
             </div>
