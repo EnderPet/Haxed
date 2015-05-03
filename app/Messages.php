6d1c00260a1d9802632257678ecc002772f74c7b
Erica <palosaari.erica@gmail.com>
2015-05-03 08:27:00 +0200

    asdasd

--- app/Messages.php
+++ app/Messages.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 class Messages extends Model {
 
     protected $table = 'messages';
-
+    public $timestamps = false;
     protected $fillable = ['tuid', 'fuid', 'message', 'title'];
 
     public function user()
