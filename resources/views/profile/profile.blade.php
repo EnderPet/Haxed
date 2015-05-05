8682b0d4656c47a14fd794514469eafd363636df
Erica <palosaari.erica@gmail.com>
2015-05-05 11:39:40 +0200

    profile update

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -5,18 +5,24 @@
         <div class="panel panel-default">
             <div class="panel-heading">Profile of {{ $profiledata->name }}</div>
             <div class="panel-body">
-                <div class="well well-sm">
-                    <p>Username: {{ $profiledata->name }}</p>
-                    <p>Level: {{ $profiledata->level }}</p>
-                    <p>Experience: {{ $profiledata->exp }} / {{ $profiledata->max_exp }}</p>
-                </div>
-                <div class="well well-sm">
-                    Money: {{ $profiledata->money }}
-                    Yeast: {{ $profiledata->yeast }}
-                    Sugar: {{ $profiledata->sugar }}
-                    Coal: {{ $profiledata->coal }}
-                    Mash: {{ $profiledata->mash }}
-                    Alcohol: {{ $profiledata->alcohol }}
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="well well-sm">
+                            <p>Username: {{ $profiledata->name }}</p>
+                            <p>Level: {{ $profiledata->level }}</p>
+                            <p>Experience: {{ $profiledata->exp }} / {{ $profiledata->max_exp }}</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="well well-sm">
+                            Money: {{ $profiledata->money }}
+                            Yeast: {{ $profiledata->yeast }}
+                            Sugar: {{ $profiledata->sugar }}
+                            Coal: {{ $profiledata->coal }}
+                            Mash: {{ $profiledata->mash }}
+                            Alcohol: {{ $profiledata->alcohol }}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
